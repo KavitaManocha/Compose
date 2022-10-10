5f1f6b849b95d47187f009c25f7a08900b242ad9
Kavita Manocha <kavita28978@gmail.com>
2022-10-10 22:35:43 +0530

    Implemented Compose Components to design Login Screen, navigate between Screens and pass values when navigating from one screen to another

---
+++ app/src/main/java/com/example/loginexample/WelcomeScreen.kt
@@ -0,0 +1,43 @@
+package com.example.loginexample
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun WelcomeScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable {
+                navController.navigate(Screen.Login.route){
+                   // In case when we want to send some data from second screen to first
+                    popUpTo(Screen.Login.route){
+                        inclusive= true
+                    }
+                }
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Welcome User",
+            color = Color.Blue,
+            fontSize = 24.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewWelcome() {
+    WelcomeScreen(navController = rememberNavController())
+}
